f219d5adc69176a66a4f272fb9c4be7dd771d574
Masl <marcel.z2006@gmail.com>
2022-05-07 23:11:42 +0200

    aaaaa

--- src/com/marcel/RACF/README.md
+++
@@ -1,7 +0,0 @@
-# RACF
-RACF - Rocc Adam Config File (Format)
-
-This is a repo for the Rocc Adam Config File Format "RACF".
-
-
-ye
--- src/com/marcel/RACF/src/com/marcel/RACF/Testing.java
+++
@@ -1,8 +0,0 @@
-package com.marcel.RACF.src.com.marcel.RACF;
-
-public class Testing {
-
-    public static void test() {
-	    // Add some code for testing ig idk
-    }
-}
